38812c3907031f0ebc66a5b9a60e3f4d3816cc4a
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-26 21:34:55 -0300

    feat: implementa previsao

--- previsao_do_tempo/index.js
+++ previsao_do_tempo/index.js
@@ -1,4 +1,24 @@
 require('dotenv').config()
+const axios = require('axios')
+// const PROTOCOL = process.env.PROTOCOL
+// const APPID = process.env.APPID
+const { 
+  PROTOCOL,
+  BASE_URL,
+  APPID,
+  Q,
+  UNITS,
+  CNT 
+} = process.env
 
-const APPID = process.env.APPID
-console.log(APPID)
+const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&q=${Q}&units=${UNITS}&cnt=${CNT}`
+
+//arrow function
+axios.get(url).then((res) => {
+  //console.log(res.data)
+  for(let previsao of res.data.list){
+    console.log(new Date(previsao.dt * 1000).toLocaleString())
+  }
+})
+
+
