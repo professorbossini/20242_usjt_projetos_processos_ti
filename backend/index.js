7a3eb6c407d6dcdbd55ba10475aef6ec66614fff
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-10 19:55:15 -0300

    feat: comeca endpoint de chatgpt

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require('express')
+const { OpenAI } = require('openai')
 const app = express()
 app.use(express.json())
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 
 //POST (criar recurso no servidor)
 //GET (obter recurso já existente no servidor)
@@ -11,6 +15,15 @@ app.get("/hello", (req, res) => {
   res.json({
     mensagem: "Hello, World com endpoints"
   })
+})
+
+//POST /pergunte-ao-chatgpt
+app.post('/pergunte-ao-chatgpt', (req, res) => {
+  const openai = new OpenAI(OPENAI_API_KEY)
+  const prompt = req.body.prompt
+  //operador de desestruturação
+  // const { prompt } = req.body
+
 })
 
 app.listen(3000, () => console.log('Servidor ok'))
