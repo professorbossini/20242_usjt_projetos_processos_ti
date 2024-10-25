d500a03a2c2fbdab110cc0c2c4fc77b2ffd236f2
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-24 21:34:29 -0300

    ap03(react): codigo 2.7.2
    - continuar na definição do componente react

--- frontend/ap03-componentes/src/index.js
+++ frontend/ap03-componentes/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   return (
-    <div style={{margin: 'auto'}}>
-
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label style={{display: 'block', marginBottom: 4}} htmlFor='nome'>Nome:</label>  
+      <input style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', outline: 'none', width: '100%', borderRadius: 8}} id="nome" type='text'/>
     </div>
   )
 }
