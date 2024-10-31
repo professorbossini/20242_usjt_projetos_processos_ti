4ea72c0928b5ca9d5d14b1d37324376806555a9b
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-31 20:12:25 -0300

    ap03(react): encerra a apostila ap07(react): cria a aplicacao

--- frontend/ap03-componentes/src/index.js
+++ frontend/ap03-componentes/src/index.js
@@ -6,6 +6,9 @@ const App = () => {
     <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
       <label style={{display: 'block', marginBottom: 4}} htmlFor='nome'>Nome:</label>  
       <input style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', outline: 'none', width: '100%', borderRadius: 8}} id="nome" type='text'/>
+      <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', borderStyle: 'hidden', width: '100%', borderRadius: 8}}>
+        Enviar
+      </button>
     </div>
   )
 }
